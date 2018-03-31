4fb0edc6165bf84f532b8d9fd88cf3ef294b8bda
Max <intmaxfactor@Maxs-MacBook-Air.local>
2018-03-31 09:32:20 +0300

    forEach response

--- xrp_finder/script.js
+++ xrp_finder/script.js
@@ -10,19 +10,23 @@ function getUrl(address,descending="true",limit="50",start="",end=""){
 }
 function getValids(){
 	var xrpAddress = ["rUocf1ixKzTuEe34kmVhRvGqNCofY1NJzV","rGDqRw6JcoRaW5xbwd5hfiQ6C89NNU2iyu"];
-	var response = {};
+	var response = [];
 	xrpAddress.forEach(function(a){
-		response = response.concat(getResponse(getUrl(xrpAddress)));
+		response.push(getResponse(getUrl(a)));
 	});
 	var valid = [];
-	response.transactions.forEach(
+	response.forEach(function (r){
+		r.transactions.forEach(
 			function(el){
 				if(
-					(el.tx.Destination===xrpAddress)&&
+					(el.tx.Destination===xrpAddress[xrpAddress.indexOf(el.tx.Destination)])&&
 					((""+el.tx.DestinationTag).length<6)
 				) 
 				valid.push(el.hash);
 			}
 		)
+
+	};
+	
 	return valid;
 }
